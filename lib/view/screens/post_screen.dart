dc1a4da3428c6ffacc723fc425b15f203e37f8b9
S <shakibmohammad18@gmail.com>
2024-08-02 20:20:28 +0600

    upgrade in firebase database/firebase storage/ intl/ image_picker add in project and successfully created and run this project no error

--- lib/view/screens/post_screen.dart
+++ lib/view/screens/post_screen.dart
@@ -1,6 +1,9 @@
-
-
+import 'dart:io';
+import 'package:blog_app_with_firebase/golobal_wieght/round_button.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:firebase_database/firebase_database.dart';
 
 class PostScreen extends StatefulWidget {
   const PostScreen({super.key});
@@ -10,6 +13,73 @@ class PostScreen extends StatefulWidget {
 }
 
 class _PostScreenState extends State<PostScreen> {
+
+
+
+  File? image;
+  final picker = ImagePicker();
+
+  TextEditingController titleController = TextEditingController();
+  TextEditingController discriptionController = TextEditingController();
+  Future getCameraImage() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+    setState(() {
+      if (pickedFile != null) {
+        image = File(pickedFile.path);
+      } else {
+        print("No Image Selected");
+      }
+    });
+  }
+
+  Future getImageGallery() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        image = File(pickedFile.path);
+      } else {
+        print("No Image Selected");
+      }
+    });
+  }
+
+  void dialog(context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext) {
+          return AlertDialog(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            content: Container(
+              height: 120,
+              child: Column(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      getCameraImage();
+                      Navigator.pop(context);
+                    },
+                    child: ListTile(
+                      leading: Icon(Icons.camera_alt),
+                      title: Text("Camera"),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      getImageGallery();
+                      Navigator.pop(context);
+                    },
+                    child: ListTile(
+                      leading: Icon(Icons.photo_library),
+                      title: Text("Gallery"),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,10 +91,85 @@ class _PostScreenState extends State<PostScreen> {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 30),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
           child: Column(
             children: [
-
+              InkWell(
+                onTap: () {
+                  dialog(context);
+                },
+                child: Center(
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * .2,
+                    width: MediaQuery.of(context).size.height * 1,
+                    child: image != null
+                        ? ClipRRect(
+                            child: Image.file(
+                              image!.absolute,
+                              height: 100,
+                              width: 100,
+                              fit: BoxFit.fill,
+                            ),
+                          )
+                        : Container(
+                            height: 100,
+                            width: 100,
+                            decoration: BoxDecoration(
+                              color: Colors.grey.shade300,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Icon(
+                              Icons.camera_alt,
+                              color: Colors.pink,
+                              size: 40,
+                            ),
+                          ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Form(
+                  child: Column(
+                children: [
+                  TextFormField(
+                    controller: titleController,
+                    decoration: InputDecoration(
+                      labelText: "title",
+                      hintText: "Enter Post Title",
+                      border: OutlineInputBorder(),
+                      hintStyle: TextStyle(
+                          color: Colors.grey, fontWeight: FontWeight.normal),
+                      labelStyle: TextStyle(
+                          color: Colors.grey, fontWeight: FontWeight.normal),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: discriptionController,
+                    minLines: 1,
+                    maxLines: 5,
+                    maxLength: 200,
+                    decoration: InputDecoration(
+                      labelText: "discription",
+                      hintText: "Enter Your Discription",
+                      border: OutlineInputBorder(),
+                      hintStyle: TextStyle(
+                          color: Colors.grey, fontWeight: FontWeight.normal),
+                      labelStyle: TextStyle(
+                          color: Colors.grey, fontWeight: FontWeight.normal),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  RoundButton(
+                      title: 'Upload', onPress: () {}, color: Colors.deepOrange)
+                ],
+              ))
             ],
           ),
         ),
